cb822141f54c5c43257d46951a13cd03a6d95185
hjhearts <60081459+hjhearts@users.noreply.github.com>
2020-04-21 14:37:47 +0900

    Init Commit

---
+++ src/main/java/ch03/spring/MemberListPrinter.java
@@ -0,0 +1,18 @@
+package ch03.spring;
+
+import java.util.Collection;
+
+public class MemberListPrinter {
+    private MemberDAO memberDAO;
+    private MemberPrinter memberPrinter;
+
+    public MemberListPrinter(MemberDAO memberDAO, MemberPrinter memberPrinter){
+        this.memberDAO = memberDAO;
+        this.memberPrinter = memberPrinter;
+    }
+
+    public void printAll(){
+        Collection<Member> members = memberDAO.selectAll();
+        members.forEach(member ->memberPrinter.print(member));
+    }
+}

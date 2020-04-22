7236366002f207896d318146f1870dd58f5be84d
hjhearts <60081459+hjhearts@users.noreply.github.com>
2020-04-22 16:40:51 +0900

    Init Commit

--- src/main/java/ch06/config/AppCtx.java
+++ src/main/java/ch06/config/AppCtx.java
@@ -5,10 +5,13 @@ import ch06.spring.Client2;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppCtx {
 
+    @Bean
+    @Scope("singleton")
     public Client client(){
         Client client = new Client();
         client.setHost("host");
---
+++ src/main/java/ch07/ExeTimeCalculator.java
@@ -0,0 +1,16 @@
+package ch07;
+
+public class ExeTimeCalculator implements Calculator {
+    private Calculator calculator;
+    public ExeTimeCalculator(Calculator calculator){
+        this.calculator = calculator;
+    }
+    @Override
+    public long factorial(long num) {
+        long start = System.nanoTime();
+        long result = calculator.factorial(num);
+        long end = System.nanoTime();
+        System.out.printf("%s.factorial(%d) 실행시간 : %d\n", calculator.getClass().getSimpleName(), num, (end-start));
+        return result;
+    }
+}
